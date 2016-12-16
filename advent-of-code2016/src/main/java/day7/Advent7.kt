9398f7013278152a6ddf3daf6dcc4b5e5d99fc6a
REDNBLACK <rednblack@protonmail.com>
2016-12-16 09:45:08 +0300

    Added not working day7 solution

---
+++ advent-of-code2016/src/main/java/day7/Advent7.kt
@@ -0,0 +1,64 @@
+package day7
+
+import isPalindrome
+import middleNChars
+import parseInput
+
+/**
+--- Day 7: Internet Protocol Version 7 ---
+
+While snooping around the local network of EBHQ, you compile a list of IP addresses (they're IPv7, of course; IPv6 is much too limited). You'd like to figure out which IPs support TLS (transport-layer snooping).
+
+An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA. An ABBA is any four-character sequence which consists of a pair of two different characters followed by the reverse of that pair, such as xyyx or abba. However, the IP also must not have an ABBA within any hypernet sequences, which are contained by square brackets.
+
+For example:
+
+abba[mnop]qrst supports TLS (abba outside square brackets).
+abcd[bddb]xyyx does not support TLS (bddb is within square brackets, even though xyyx is outside square brackets).
+aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters must be different).
+ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a larger string).
+How many IPs in your puzzle input support TLS?
+
+ */
+
+fun main(args: Array<String>) {
+    val test = """abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn
+"""
+
+    println(findSupportingTLS(test))
+
+    val input = parseInput("day7-input.txt")
+    println(findSupportingTLS(input).size)
+}
+
+data class IP(val sequences: List<String>, val hypernet: List<String>) {
+    fun supportsTLS(): Boolean {
+        if (hypernet.any { it.middleNChars(4).isPalindrome() }) return false
+
+        return sequences.any { it.middleNChars(4).isPalindrome() }
+    }
+
+    companion object {
+        val hypernetPattern = """\[(.+?)\]""".toRegex()
+        val sequencePattern = """.(?![^\[]*\])""".toRegex()
+
+        fun fromString(str: String): IP {
+            val sequences = sequencePattern.findAll(str).map { it.groupValues[0] }.joinToString("").split("]")
+            val hypernets = hypernetPattern.findAll(str).map { it.groupValues[1] }.toList()
+
+            return IP(sequences, hypernets)
+        }
+    }
+}
+
+fun findSupportingTLS(input: String) = parseIP(input).filter(IP::supportsTLS)
+
+fun parseIP(input: String): List<IP> {
+    return input.split("\n")
+            .map(String::trim)
+            .filter(String::isNotEmpty)
+            .map { IP.fromString(it) }
+}
--- advent-of-code2016/src/main/java/utils.kt
+++ advent-of-code2016/src/main/java/utils.kt
@@ -16,3 +16,21 @@ fun <T> List<T>.chunk(size: Int): List<List<T>> {
 
 inline fun <reified INNER> array2d(sizeOuter: Int, sizeInner: Int, noinline innerInit: (Int) -> INNER): Array<Array<INNER>>
         = Array(sizeOuter) { Array(sizeInner, innerInit) }
+
+fun String.isPalindrome(): Boolean {
+    val length = this.length / 2
+    val firstHalf = this.substring(0, length)
+    val secondHalf = this.substring(length)
+
+    if (firstHalf.groupBy { it }.size == 1) return false
+
+    return firstHalf == secondHalf.reversed()
+}
+
+fun String.middleNChars(n: Int): String {
+    val middle = this.length / 2
+    val first = this.substring(middle - n / 2, middle)
+    val second = this.substring(middle, middle + n / 2)
+
+    return first + second
+}
