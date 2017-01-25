5a88985eeeab609f8bb9453ae4175ad3da206df0
REDNBLACK <rednblack@protonmail.com>
2017-01-25 20:58:17 +0300

    Added working solution for day15-1 and not working for day15-2

--- advent-of-code2015/src/main/kotlin/day15/Advent15.kt
+++ advent-of-code2015/src/main/kotlin/day15/Advent15.kt
@@ -30,6 +30,14 @@ Multiplying these together (68 * 80 * 152 * 76, ignoring calories for now) resul
 
 Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you can make?
 
+--- Part Two ---
+
+Your cookie recipe becomes wildly popular! Someone asks if you can make another recipe that has exactly 500 calories per cookie (so they can use it as a meal replacement). Keep the rest of your award-winning process the same (100 teaspoons, same ingredients, same scoring system).
+
+For example, given the ingredients above, if you had instead selected 40 teaspoons of butterscotch and 60 teaspoons of cinnamon (which still adds to 100), the total calorie count would be 40*8 + 60*3 = 500. The total score would go down, though: only 57600000, the best you can do in such trying circumstances.
+
+Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you can make with a calorie total of 500?
+
  */
 
 fun main(args: Array<String>) {
@@ -39,10 +47,14 @@ fun main(args: Array<String>) {
                """.trimMargin()
     val input = parseInput("day15-input.txt")
 
-    println(findBestCombination(test))
+    println(findBestCombination(test) == 62842880)
+    println(findBestCombination(test, maxCalories = 500) == 57600000)
+
+    println(findBestCombination(input))
+    println(findBestCombination(input, maxCalories = 500))
 }
 
-fun findBestCombination(input: String): Int? {
+fun findBestCombination(input: String, maxTeaspoons: Int = 100, maxCalories: Int = 0): Int? {
     val ingredients = parseIngredients(input)
 
     fun mixtures(size: Int, max: Int, acc: MutableList<List<Int>> = mutableListOf()): List<List<Int>> {
@@ -60,17 +72,18 @@ fun findBestCombination(input: String): Int? {
         return acc
     }
 
-    fun score(recipe: List<Int>): Int {
-        val proportions = ingredients.map { it.toList() }.zip(recipe).map { p -> p.first.map { it * p.second } }
-        val dough = proportions.reduce { a, b -> a.zip(b).map { it.toList().sum() } }.dropLast(1)
-        val result = dough.map { Math.max(it, 0) }.reduce { a, b -> a * b }
+    fun score(recipe: List<Int>, maxCalories: Int): Int {
+        val proportions = ingredients.map { it.toList() }
+                .zip(recipe)
+                .map { p -> p.first.map { it * p.second } }
+        val dough = proportions.reduce { a, b -> a.zip(b).map { it.toList().sum() } }
+        val calories = dough.last()
+        val result = dough.dropLast(1).map { Math.max(it, 0) }.reduce { a, b -> a * b }
 
-        // 62_842_880
-        // 15_020_544
-        return result
+        return if (maxCalories != 0 && calories > maxCalories) 0 else result
     }
 
-    return mixtures(ingredients.size, 100).map(::score).max()
+    return mixtures(ingredients.size, maxTeaspoons).map { score(it, maxCalories) }.max()
 }
 
 data class Ingredient(
@@ -81,7 +94,7 @@ data class Ingredient(
         val texture: Int,
         val calories: Int
 ) {
-    fun toList() = listOf(capacity, durability, flavor, texture)
+    fun toList() = listOf(capacity, durability, flavor, texture, calories)
 }
 
 private fun parseIngredients(input: String) = input
@@ -90,7 +103,7 @@ private fun parseIngredients(input: String) = input
         .filter(String::isNotEmpty)
         .map {
             val name = it.split(":", limit = 2).first()
-            val (capacity, durability, flavor, texture, calories) = Regex("""(\d+)""")
+            val (capacity, durability, flavor, texture, calories) = Regex("""(-?\d+)""")
                     .findAll(it)
                     .map { it.groupValues[1] }
                     .toList()
