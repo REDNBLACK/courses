644f56a85793f842ad1639fedce562aaa8863b5d
REDNBLACK <rednblack@protonmail.com>
2017-02-14 11:20:50 +0300

    Added working solution for day21-1

--- advent-of-code2016/src/main/kotlin/utils.kt
+++ advent-of-code2016/src/main/kotlin/utils.kt
@@ -19,6 +19,25 @@ fun <T> List<T>.chunk(size: Int): List<List<T>> {
         subList(fromIndex, toIndex)
     }
 }
+fun String.swap(a: Int, b: Int) = toCharArray()
+        .apply {
+            val tmp = this[a]
+            this[a] = this[b]
+            this[b] = tmp
+        }
+        .joinToString("")
+fun String.move(from: Int, to: Int) = toMutableList().apply { add(to, removeAt(from)) }.joinToString("")
+fun String.rotate(times: Int = 1) = (1..Math.abs(times))
+        .fold(this, { s, _i ->
+            if (times > 0) {
+                s.substring(1) + s.substring(0, 1)
+            } else {
+                s.substring(length - 1, length) + s.substring(0, length - 1)
+            }
+        })
+fun String.reverseSubstring(from: Int, to: Int) = substring(0, from) +
+        substring(from..to).reversed() +
+        substring(to + 1, length)
 
 fun String.chunk(size: Int) = (0..length - size).map { i -> substring(i, i + size) }
 
