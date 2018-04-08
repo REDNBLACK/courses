d6801ea1d0c6c0e4bd6283c969d0d7f530f7a123
Boris <boris@f0w.org>
2018-04-08 22:57:57 +0300

    Added not solved scala-koans

---
+++ scala-koans/src/test/scala/org/functionalkoans/forscala/support/KoanSuite.scala
@@ -0,0 +1,56 @@
+package org.functionalkoans.forscala.support
+
+import org.scalatest.exceptions.TestPendingException
+import org.scalatest._
+import org.scalatest.matchers.Matcher
+import org.scalatest.events.{Event, TestFailed, TestIgnored, TestPending}
+
+trait KoanSuite extends FunSuite with Matchers {
+
+  def koan(name : String)(fun: => Unit) { test(name.stripMargin('|'))(fun) }
+
+  def meditate() = pending
+
+  def  __ : Matcher[Any] = {
+    throw new TestPendingException
+  }
+
+  protected class ___ extends Exception {
+    override def toString = "___"
+  }
+
+  private class ReportToTheMaster(other: Reporter) extends Reporter {
+    var failed = false
+    def failure(event: Master.HasTestNameAndSuiteName) {
+      failed = true
+      info("*****************************************")
+      info("*****************************************")
+      info("")
+      info("")
+      info("")
+      info(Master.studentFailed(event))
+      info("")
+      info("")
+      info("")
+      info("*****************************************")
+      info("*****************************************")
+    }
+
+    def apply(event: Event) {
+      event match {
+        case e: TestIgnored => failure(event.asInstanceOf[Master.HasTestNameAndSuiteName])
+        case e: TestFailed => failure(event.asInstanceOf[Master.HasTestNameAndSuiteName])
+        case e: TestPending => failure(event.asInstanceOf[Master.HasTestNameAndSuiteName])
+        case _ => other(event)
+      }
+
+    }
+  }
+
+  protected override def runTest(testName: String, args: Args) = {
+    if (!Master.studentNeedsToMeditate) {
+      super.runTest(testName, args.copy(reporter = new ReportToTheMaster(args.reporter)))
+    }
+    SucceededStatus
+  }
+}
