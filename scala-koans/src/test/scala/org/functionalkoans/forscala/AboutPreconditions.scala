23bec0208ed0902d687f392914ac749fde1bd654
Boris <boris@f0w.org>
2018-04-09 14:44:40 +0300

    Added solution for AboutPreconditions

--- scala-koans/src/test/scala/org/functionalkoans/forscala/AboutPreconditions.scala
+++ scala-koans/src/test/scala/org/functionalkoans/forscala/AboutPreconditions.scala
@@ -3,7 +3,6 @@ package org.functionalkoans.forscala
 import support.KoanSuite
 
 class AboutPreconditions extends KoanSuite {
-
   class WithParameterRequirement(val myValue: Int) {
     require(myValue != 0)
 
@@ -15,9 +14,9 @@ class AboutPreconditions extends KoanSuite {
   // Instruction: use Intercept to catch the type of exception thrown by an invalid precondition
   koan("On precondition violation, intercept expects type of exception thrown") {
     val myInstance = new WithParameterRequirement("Do you really like my hair?")
-    myInstance.myValue should be (__)
+    myInstance.myValue should be(27)
 
-    intercept[___] { //Catching the requirement exception, enter the exception in ___
+    intercept[IllegalArgumentException] { //Catching the requirement exception, enter the exception in ___
       new WithParameterRequirement("")
     }
   }
