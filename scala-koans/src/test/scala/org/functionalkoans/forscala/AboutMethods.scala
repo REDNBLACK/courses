9ba7ae716c3ff8a3b214d636c3a669c701910a8f
Boris <boris@f0w.org>
2018-04-09 13:12:48 +0300

    Added solution for AboutMethods

--- scala-koans/src/test/scala/org/functionalkoans/forscala/AboutMethods.scala
+++ scala-koans/src/test/scala/org/functionalkoans/forscala/AboutMethods.scala
@@ -5,7 +5,6 @@ import org.functionalkoans.forscala.support.KoanSuite
 import scala.annotation.tailrec
 
 class AboutMethods extends KoanSuite {
-
   koan( """A method's last statement will be what is returned
           | There is no need for the keyword `return`.
           | When a method includes a `=` after the method declaration that
@@ -13,7 +12,7 @@ class AboutMethods extends KoanSuite {
     def add(x: Int, y: Int) = {
       x + y
     }
-    add(6, 7) should be(__)
+    add(6, 7) should be(13)
   }
 
   koan(
@@ -23,30 +22,28 @@ class AboutMethods extends KoanSuite {
       //Notice the :Int at the end of the method
       x + y
     }
-    add(2, 10) should be(__)
+    add(2, 10) should be(12)
   }
-
 
   koan(
     """If a method returns two different types and no explicit
       | return type is defined,
       | the type of the method will likely be inferred as the
       | common super parent of the two types being returned""") {
-
     def add(x: Int, y: Int) = {
       //implicitly the return type is Any
       if (x > 10) (x + y).toString
       else x + y
     }
 
-    add(2, 10) should be(__)
-    add(1, 1) should be(__)
+    add(2, 10) should be(12)
+    add(1, 1) should be(2)
   }
 
 
   koan( """If a method does not of have equal it is considered `Unit` which is analogous to `void` in Java""") {
     def foo(x: Int) { //Note: No `=`
-      (x + 4) should be(__)
+      (x + 4) should be(9)
     }
     foo(5)
   }
@@ -55,19 +52,19 @@ class AboutMethods extends KoanSuite {
     """If you want to have an = on the method, while still explicitly returning Unit you can make the return type `Unit`,
       | this also analogous to `void""") {
     def foo(x: Int): Unit = { //Note we are declaring Unit
-      (x + 4) should be(__)
+      (x + 4) should be(7)
     }
     foo(3)
   }
 
   koan( """Once you have an =, it is understood that there will be a return type and can be inferred""") {
     def foo(x: Int) = 3 + 4
-    foo(3).isInstanceOf[Int] should be(__) //.isInstanceOf[...] is analogous to Java's instanceOf
+    foo(3).isInstanceOf[Int] should be(true) //.isInstanceOf[...] is analogous to Java's instanceOf
   }
 
   koan( """Of course if you wish to be explicit about the return type, you can attach it at the end of the method""") {
     def foo(x: Int): Int = 3 + 4
-    foo(3).isInstanceOf[Int] should be(__)
+    foo(3).isInstanceOf[Int] should be(true)
   }
 
   koan(
@@ -79,7 +76,7 @@ class AboutMethods extends KoanSuite {
       else x * factorial(x-1)
     }
 
-    factorial(4) should be(__) // List(...) is how a list is created more about lists later.
+    factorial(4) should be(24) // List(...) is how a list is created more about lists later.
 
     //Note: Fire up a REPL and paste factorial(100000)!
   }
@@ -104,7 +101,7 @@ class AboutMethods extends KoanSuite {
       fact(i, 1)
     }
 
-    factorial(4) should be(__)
+    factorial(4) should be(24)
 
     //Note: Fire up a REPL and try factorial(100000) now!
   }
@@ -130,17 +127,15 @@ class AboutMethods extends KoanSuite {
   koan(
     """Remember you can have strange characters in values and variables as long as they're
       |  after an underscore, well you can do the same in methods""") {
-
     class Pennies(val n:Int)
     def doYouHaveAnySpareChange_?() = new Pennies(25)
-    doYouHaveAnySpareChange_?.n should be(__)
+    doYouHaveAnySpareChange_?.n should be(25)
   }
 
   koan(
     """If you also remember you can add reserved words or words with space to any value or variable?
       | Same applies for methods. Although please note that this is uncommon unless you really are into
       | internal DSLs""") {
-
     class Employee(val `first name`:String, val `last name`:String, val `employee status`: String)
 
     def `put employee on probation`(employee: Employee) = {
@@ -148,7 +143,7 @@ class AboutMethods extends KoanSuite {
     }
 
     val probationEmployee = `put employee on probation`(new Employee("Milton", "Waddams", ""))
-    probationEmployee.`employee status` should be (__)
+    probationEmployee.`employee status` should be("Probation")
   }
 
   koan(
@@ -158,7 +153,7 @@ class AboutMethods extends KoanSuite {
     def add(a:Int, b:Int) = a + b //implied return type of Int!
     def performSideEffect():Unit = System.currentTimeMillis
 
-    add(4,6) should be (__)
+    add(4,6) should be (10)
     performSideEffect() //Notice the parenthesis, since the method we called is Unit!
   }
 
@@ -171,7 +166,7 @@ class AboutMethods extends KoanSuite {
     }
 
     val foo = new Foo(9)
-    10 ~: foo should be (__)
-    foo.~:(40) should be (__)
+    10 ~: foo should be (22)
+    foo.~:(40) should be (52)
   }
 }
