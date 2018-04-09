a1bac9f505cf39c339e69cc78512dceee86a5a85
Boris <boris@f0w.org>
2018-04-09 12:10:21 +0300

    Added solution for AboutInfixTypes

--- scala-koans/src/test/scala/org/functionalkoans/forscala/AboutInfixTypes.scala
+++ scala-koans/src/test/scala/org/functionalkoans/forscala/AboutInfixTypes.scala
@@ -4,7 +4,6 @@ import org.scalatest.Matchers
 import support.KoanSuite
 
 class AboutInfixTypes extends KoanSuite with Matchers {
-
   koan("""We can make a type infix, meaning that the type can be displayed in complement
            between two types in order to make a readable delaration""") {
     case class Person(name: String)
@@ -15,15 +14,14 @@ class AboutInfixTypes extends KoanSuite with Matchers {
       couple.a.name + " is in love with " + couple.b.name
     }
 
-    val romeo = new Person("Romeo")
-    val juliet = new Person("Juliet")
+    val romeo = Person("Romeo")
+    val juliet = Person("Juliet")
 
-    announceCouple(new Loves(romeo, juliet)) should be(__)
+    announceCouple(new Loves(romeo, juliet)) should be("Romeo is in love with Juliet")
   }
 
   koan("""Of course we can make this a bit more elegant by creating an infix operator
            |  method to use with our infix type""") {
-
     case class Person(name: String) {
       def loves(person: Person) = new Loves(this, person)
     }
@@ -35,11 +33,9 @@ class AboutInfixTypes extends KoanSuite with Matchers {
       couple.a.name + " is in love with " + couple.b.name
     }
 
-    val romeo = new Person("Romeo")
-    val juliet = new Person("Juliet")
+    val romeo = Person("Romeo")
+    val juliet = Person("Juliet")
 
-    announceCouple(romeo loves juliet) should be(__)
+    announceCouple(romeo loves juliet) should be("Romeo is in love with Juliet")
   }
-
-
 }
